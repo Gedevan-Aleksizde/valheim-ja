9f26de58c49d60dfcd46446fbac92aa2f5c8f247
Satoshi Katagiri <katagiri.stsh@gmail.com>
2021-09-20 11:54:07 +0900

    ???

--- tools/read.py
+++ tools/read.py
@@ -5,35 +5,37 @@ import pandas as pd
 
 # NOT in tools/
 rootdir = Path().cwd()
-jsonpath = rootdir.joinpath('localization-resources.assets-99-TextAsset.json').expanduser()
+
+l10n_jsonpath = [rootdir.joinpath(x).expanduser() for x in [
+    'localization-resources.assets-99-TextAsset.json',
+    'localization_extra-resources.assets-100-TextAsset.json'
+]]
 excelpath = rootdir.joinpath('l10n.xlsx').expanduser()
 csvpath = rootdir.joinpath('l10n-native.csv').expanduser()
+csvs = []
+for x in l10n_jsonpath:
+    with x.open("r") as f:
+        tmp = pd.read_csv(StringIO(json.load(f)['0 TextAsset Base']['1 string m_Script']), encoding="utf-8").fillna('').rename(columns={'Context': ' '})
+        csvs += [tmp.assign(OriginalFileName=x.name)]
+    # In v0.202.19, l10n-extra text assets have field names ['Context', 'English', ..., 'Japanese', ...] while l10n text assets have ['', 'English', ..., 'Japanese', ...] 
+    # variable symbols have `$` in prefix, so we can search by regex "\$[0-9a-zA-Z]+"
+    # automaticalyy tagging
 
-with jsonpath.open("r") as f:
-    l10n = json.load(f)
-    tab_l10n = pd.read_csv(StringIO(l10n['0 TextAsset Base']['1 string m_Script']), encoding="utf-8").fillna('')
-
-tab_l10n.info()
-tab_l10n[[' ', 'English', 'Japanese']]
-
-tab_l10n.loc[lambda d: d['Japanese'].str.contains("\$[0-9a-zA-Z]+"), 'Japanese']
-
-# tagging しないと変数が置き換えられない
-tab_l10n = tab_l10n.assign(
-    Japanese = lambda d: d['Japanese'].str.replace(
-        '(\$[0-9a-zA-Z]+)', ' \\1 ', regex=True
-    ).str.replace(
-        '^\s', '', regex=True
-    ).str.replace(
-        '\s$', '', regex = True
-    ).str.replace(
-        '\s{2,}', ' ', regex=True
+tab_l10n = pd.concat(csvs)
+count = tab_l10n.loc[lambda d: d['Japanese'].str.match('\$[0-9a-zA-Z]+')].shape[0]
+print(f"{count} invalid tagging are found")
+if(count > 0):
+    tab_l10n = (
+        tab_l10n.assign(Japanese = lambda d: d['Japanese'].str.replace('(\$[0-9a-zA-Z]+)', ' \\1 ', regex=True).
+        str.replace('^\s', '', regex=True).
+        str.replace('\s$', '', regex = True).
+        str.replace('\s{2,}', ' ', regex=True)
+        )
     )
-)
 # quoting=1: csv.QUOTE_ALL
 tab_l10n.to_csv(csvpath, index=False, encoding="utf-8", quoting=1)
 # 手動編集の簡易さからエクセルに書く
 # TODO: libre が重すぎるので PO ファイルとして出力したほうがいいか?
 with pd.ExcelWriter(excelpath) as writer:  
     tab_l10n.to_excel(writer, sheet_name = "mod", index=False)
-    tab_l10n.to_excel(writer, sheet_name = "v0.156.2", index=False)
+    tab_l10n.to_excel(writer, sheet_name = "v0.202.19", index=False)
--- tools/write.py
+++ tools/write.py
@@ -8,33 +8,35 @@ import pandas as pd
 rootdir = Path().cwd()
 excelpath = rootdir.joinpath('l10n.xlsx').expanduser()
 csvpath = rootdir.cwd().joinpath('l10n-mod.csv').expanduser()
-outputpath = [rootdir.joinpath(f'output/{x}').expanduser() for x in [
+outputname = {x.split('.')[0].split('-')[0] : rootdir.joinpath(f'output/{x}').expanduser() for x in [
     'localization-resources.assets-99-TextAsset.txt',
     'localization_extra-resources.assets-100-TextAsset.txt'
-]]
+]}
 
 tab_l10n = pd.read_excel(excelpath, sheet_name='mod').fillna('')
 # export CSV to compare the native text
 tab_l10n.to_csv(csvpath, index=False, encoding="utf-8", quoting=1)
 
-tab_l10n.loc[lambda d: [Path(x).with_suffix('').name==Path('localization_extra-resources.assets-100-TextAsset.txt').with_suffix('') for x in d['OriginalFileName']]].drop(columns=['OriginalFileName'])
+tab_l10n.loc[
+    lambda d: [Path(x).with_suffix('').name==Path('localization_extra-resources.assets-100-TextAsset.txt').with_suffix('') for x in d['OriginalFileName']]
+    ].drop(columns=[['OriginalFileName', 'editted']])
 [Path(x).with_suffix('').name for x in tab_l10n.OriginalFileName[1:10]]
 
-for output in outputpath:
-    tmp = tab_l10n.loc[lambda d: [Path(output).with_suffix('').name==Path(x).with_suffix('').name for x in d['OriginalFileName']]]
-    print(f'{output} has {tmp.shape[0]} entries.')
-    if output=='localization_extra-resources.assets-100-TextAsset.txt':
+for out_entry, out_path in outputname.items():
+    tmp = tab_l10n.loc[lambda d: [Path(out_path).with_suffix('').name==Path(x).with_suffix('').name for x in d['OriginalFileName']]].drop(columns=['OriginalFileName', 'Editted'])
+    print(f'{out_entry} has {tmp.shape[0]} entries.')
+    if out_entry=='localization_extra':
         tmp = tmp.rename(columns={' ': 'Context'})
     # Some entries has linebreak letters
     # # TODO: But does \r make sence?
     l10n_data = tmp.to_csv(index=False, encoding="utf-8", quoting=1, line_terminator=r"\n")
     l10n_data = l10n_data.replace('\r', r'\r').replace('\n', r'\n')
     # TODO: "import json" is useless...
-    txt = """0 TextAsset Base
-    1 string m_Name = "localization"
+    txt = f"""0 TextAsset Base
+    1 string m_Name = "{out_entry}"
     """ + f' 1 string m_Script = "{l10n_data}"\n'
 
-    if not x.parent.exists():
-        output.parent.mkdir(parents=True, exist_ok=True)
-    with output.open('w') as f:
+    if not out_path.parent.exists():
+        out_path.parent.mkdir(parents=True, exist_ok=True)
+    with out_path.open('w') as f:
         f.writelines(txt)
