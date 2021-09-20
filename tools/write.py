a05f63d1994b9916636fc9083247d01fb8dcd52e
Satoshi Katagiri <katagiri.stsh@gmail.com>
2021-09-20 11:42:04 +0900

    remove FileName info.

--- tools/write.py
+++ tools/write.py
@@ -8,20 +8,33 @@ import pandas as pd
 rootdir = Path().cwd()
 excelpath = rootdir.joinpath('l10n.xlsx').expanduser()
 csvpath = rootdir.cwd().joinpath('l10n-mod.csv').expanduser()
-txtpath = rootdir.cwd().joinpath('l10n-mod.txt').expanduser()
+outputpath = [rootdir.joinpath(f'output/{x}').expanduser() for x in [
+    'localization-resources.assets-99-TextAsset.txt',
+    'localization_extra-resources.assets-100-TextAsset.txt'
+]]
 
 tab_l10n = pd.read_excel(excelpath, sheet_name='mod').fillna('')
 # export CSV to compare the native text
 tab_l10n.to_csv(csvpath, index=False, encoding="utf-8", quoting=1)
 
-l10n_data = tab_l10n.to_csv(index=False, encoding="utf-8", quoting=1, line_terminator=r"\n")
-# セル内に改行コードが入る場合もあるため
-# TODO: しかし \r は意味があるのだろうか?
-l10n_data = l10n_data.replace('\r', r'\r').replace('\n', r'\n')
+tab_l10n.loc[lambda d: [Path(x).with_suffix('').name==Path('localization_extra-resources.assets-100-TextAsset.txt').with_suffix('') for x in d['OriginalFileName']]].drop(columns=['OriginalFileName'])
+[Path(x).with_suffix('').name for x in tab_l10n.OriginalFileName[1:10]]
 
-txt = """0 TextAsset Base
- 1 string m_Name = "localization"
-""" + f' 1 string m_Script = "{l10n_data}"\n'
+for output in outputpath:
+    tmp = tab_l10n.loc[lambda d: [Path(output).with_suffix('').name==Path(x).with_suffix('').name for x in d['OriginalFileName']]]
+    print(f'{output} has {tmp.shape[0]} entries.')
+    if output=='localization_extra-resources.assets-100-TextAsset.txt':
+        tmp = tmp.rename(columns={' ': 'Context'})
+    # Some entries has linebreak letters
+    # # TODO: But does \r make sence?
+    l10n_data = tmp.to_csv(index=False, encoding="utf-8", quoting=1, line_terminator=r"\n")
+    l10n_data = l10n_data.replace('\r', r'\r').replace('\n', r'\n')
+    # TODO: "import json" is useless...
+    txt = """0 TextAsset Base
+    1 string m_Name = "localization"
+    """ + f' 1 string m_Script = "{l10n_data}"\n'
 
-with txtpath.open('w') as f:
-    f.writelines(txt)
+    if not x.parent.exists():
+        output.parent.mkdir(parents=True, exist_ok=True)
+    with output.open('w') as f:
+        f.writelines(txt)
