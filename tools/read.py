cd86777beee3247ba7ab60bf98eb3852c71cb513
Satoshi Katagiri <katagiri.stsh@gmail.com>
2021-09-20 16:42:36 +0900

    update scripts

--- tools/read.py
+++ tools/read.py
@@ -1,30 +1,38 @@
+#! /usr/bin/env python 
+
 import json
 from pathlib import Path
 from io import StringIO
 import pandas as pd
+import argparse
 
-# NOT in tools/
+# TODO: how to extract localization texts from `resources.assets`
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--filenames', type=Path, nargs='+', help='file path(s) to parse', default=None)
+parser.add_argument('--outdir', type=Path, help='output directory', default=None)
+args = parser.parse_args(None if __name__ == '__main__' else '')
 rootdir = Path().cwd()
-
 l10n_jsonpath = [rootdir.joinpath(x).expanduser() for x in [
     'localization-resources.assets-99-TextAsset.json',
     'localization_extra-resources.assets-100-TextAsset.json'
-]]
-excelpath = rootdir.joinpath('l10n.xlsx').expanduser()
-csvpath = rootdir.joinpath('l10n-native.csv').expanduser()
+    ]
+] if args.filenames is None else args.filenames
+excelpath = rootdir.joinpath('l10n.xlsx').expanduser() if args.outdir is None else Path(args.outdir).joinpath('l10n.xlsx')
+csvpath = rootdir.joinpath('l10n-native.csv').expanduser() if args.outdir is None else Path(args.outdir).joinpath('l10n-native.csv')
 csvs = []
 for x in l10n_jsonpath:
     with x.open("r") as f:
         tmp = pd.read_csv(StringIO(json.load(f)['0 TextAsset Base']['1 string m_Script']), encoding="utf-8").fillna('').rename(columns={'Context': ' '})
+        count_without_blank = tmp.loc[lambda d: d['Japanese'].str.match('\$[0-9a-zA-Z]+')].shape[0]
+        print(f"{x}: {tmp.shape[0]} entries included; {count_without_blank} blank-omitted entries are found.")
         csvs += [tmp.assign(OriginalFileName=x.name)]
     # In v0.202.19, l10n-extra text assets have field names ['Context', 'English', ..., 'Japanese', ...] while l10n text assets have ['', 'English', ..., 'Japanese', ...] 
     # variable symbols have `$` in prefix, so we can search by regex "\$[0-9a-zA-Z]+"
     # automaticalyy tagging
 
 tab_l10n = pd.concat(csvs)
-count = tab_l10n.loc[lambda d: d['Japanese'].str.match('\$[0-9a-zA-Z]+')].shape[0]
-print(f"{count} invalid tagging are found")
-if(count > 0):
+if(tab_l10n.shape[0] > 0):
     tab_l10n = (
         tab_l10n.assign(Japanese = lambda d: d['Japanese'].str.replace('(\$[0-9a-zA-Z]+)', ' \\1 ', regex=True).
         str.replace('^\s', '', regex=True).
--- tools/write.py
+++ tools/write.py
@@ -4,34 +4,41 @@ import json
 from pathlib import Path
 from io import StringIO
 import pandas as pd
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--filename', type=Path, help='file path(s) to parse', default=None)
+parser.add_argument('--outdir', type=Path, help='output directory', default=None)
+args = parser.parse_args(None if __name__ == '__main__' else '')
 
 rootdir = Path().cwd()
-excelpath = rootdir.joinpath('l10n.xlsx').expanduser()
-csvpath = rootdir.cwd().joinpath('l10n-mod.csv').expanduser()
-outputname = {x.split('.')[0].split('-')[0] : rootdir.joinpath(f'output/{x}').expanduser() for x in [
-    'localization-resources.assets-99-TextAsset.txt',
-    'localization_extra-resources.assets-100-TextAsset.txt'
-]}
+excelpath = rootdir.joinpath('l10n.xlsx').expanduser() if args.filename is None else Path(args.flename)
+csvpath = excelpath.parent.joinpath('l10n-mod.csv').expanduser()
+outputdir = rootdir.joinpath('output') if args.outdir is None else Path(args.outdir)
+if not outputdir.exists():
+    outputdir.parent.mkdir(parents=True, exist_ok=True)
+    print(f"{outputdir} doesn't exist. Created now.")
 
 tab_l10n = pd.read_excel(excelpath, sheet_name='mod').fillna('')
 # export CSV to compare the native text
 tab_l10n.to_csv(csvpath, index=False, encoding="utf-8", quoting=1)
 
+outputname = {x.split('.')[0].split('-')[0] : outputdir.joinpath(x).expanduser() for x in tab_l10n.OriginalFileName.unique()}
+    
+
 for out_entry, out_path in outputname.items():
     tmp = tab_l10n.loc[lambda d: [Path(out_path).with_suffix('').name==Path(x).with_suffix('').name for x in d['OriginalFileName']]].drop(columns=['OriginalFileName', 'Editted'])
-    print(f'{out_entry} has {tmp.shape[0]} entries.')
     if out_entry=='localization_extra':
         tmp = tmp.rename(columns={' ': 'Context'})
     # Some entries has linebreak letters
     # # TODO: But does \r make sence?
     l10n_data = tmp.to_csv(index=False, encoding="utf-8", quoting=1, line_terminator=r"\n")
     l10n_data = l10n_data.replace('\r', r'\r').replace('\n', r'\n')
-    # TODO: "import json" is useless...
+    # TODO: "import json" is useless in UABE...
     txt = f"""0 TextAsset Base
     1 string m_Name = "{out_entry}"
     """ + f' 1 string m_Script = "{l10n_data}"\n'
-
-    if not out_path.parent.exists():
-        out_path.parent.mkdir(parents=True, exist_ok=True)
     with out_path.open('w') as f:
         f.writelines(txt)
+        print(f'{out_entry} has {tmp.shape[0]} entries and is saved at {out_path}')
