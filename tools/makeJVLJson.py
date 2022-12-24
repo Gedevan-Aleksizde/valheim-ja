2859a1a835340ec3699868c17c91afe04cb073f9
S-Katagiri <katagiri.stsh@gmail.com>
2022-12-24 19:24:28 +0900

    to connect with Unity

--- tools/makeJVLJson.py
+++ tools/makeJVLJson.py
@@ -15,7 +15,7 @@ with (Path(__file__).parent if '__file__' in locals() else Path().cwd().joinpath
         args.out = Path(f"dict/CorrectedText_{params['LANG']}.json")
     else:
         if(args.out is None):
-            args.out = Path(f"dict/CorretedText_{params['LANG']}.json")
+            args.out = Path(f"output/CorretedText_{params['LANG']}.json")
 
 with args.dict.open("r", encoding="utf-8") as f:
     dict_with_metadata = json.load(f)
@@ -26,3 +26,10 @@ dict1 = {k:v[params["LANG"]]["text"] for l in dict_with_metadata.values() for (k
 with args.out.open("w", encoding="utf-8") as f:
     json.dump(dict1, f, ensure_ascii=False, indent=2)
     print(f"json file is exported at {args.out}")
+unity_path = Path("Unity/Assets/Text")
+if not unity_path.exists:
+    unity_path.mkdir(exist_ok=True)
+with unity_path.joinpath(args.out.name) as fp:
+    with fp.open("w", encoding="utf-8") as f:
+        json.dump(dict1, f, ensure_ascii=False, indent=2)
+    print(f"json file is exported at {fp}")
