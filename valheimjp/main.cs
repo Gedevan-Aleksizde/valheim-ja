b86de46982a0afedd41525bc82461fc617b6b6fe
S-Katagiri <katagiri.stsh@gmail.com>
2022-12-24 19:26:11 +0900

    for release

--- valheimjp/main.cs
+++ valheimjp/main.cs
@@ -9,12 +9,13 @@ using Jotunn.Managers;
 
 namespace ValheimJP
 {
-    [BepInPlugin("com.bepinex.plugins.ValheimJP", "Valheim Correct Japanese Text", "1.0.0")]
+    [BepInPlugin(PluginGUID, "Corrected Japanese Text of Valheim", "1.0.0")]
     [BepInDependency(Jotunn.Main.ModGuid)]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
     [BepInDependency("cinnabun.backpacks-v1.0.0", BepInDependency.DependencyFlags.SoftDependency)]
     public class TextCorrecter : BaseUnityPlugin
     {
+        public const string PluginGUID = "Gedevan-Aleksizde.mods.ValheimJP";
         private void Awake()
         {
             AddLocalizations();
